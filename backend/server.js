c84e4c27df82ded9401127f02fb75093a8751caf
Jeremy Holland <jpholland05@gmail.com>
2023-05-09 13:27:14 -0400

    server will now send messages using twilio

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,36 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cardRoutes = require('./routes/cards')
+const schedule = require('node-schedule')
 
 // Express App
 const app = express()
+
+// Twilio
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken)
+
+const everyDayAtNoon = new schedule.RecurrenceRule()
+everyDayAtNoon.tz = 'America/New_York'
+everyDayAtNoon.hour = 12
+everyDayAtNoon.minute = 0
+
+const sendMessage = schedule.scheduleJob(everyDayAtNoon, () => {
+  message('You have reviews waiting for you in your study app', '+18335013802', '+12404096467')
+})
+
+// from: '+18335013802',
+
+function message(message, from, to) {
+  client.messages
+    .create({
+      body: message,
+      from: `${from}`,
+      to: `${to}`
+    })
+    .then(message => console.log(message.sid));
+}
 
 // Middleware
 app.use(express.json())
